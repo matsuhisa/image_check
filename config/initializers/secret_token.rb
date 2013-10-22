32d69559356715fc95afebbbe4aee215820d07ec
matsuhisa.hironobu <matsuhisa.hironobu@mwed.co.jp>
2013-10-22 19:39:58 +0900

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ImageCheck::Application.config.secret_token = '30e5d22eeccfe1567598448eb106b5312f430886207d210f3a1bda741dc4a33e5f72856d120ed74d9508e285e9e654ba462565b1205f6c136f7d51e43bdf3bf8'
